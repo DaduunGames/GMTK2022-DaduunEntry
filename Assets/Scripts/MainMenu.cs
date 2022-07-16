c43e54b9ec4c3e90a2e16425e07c49d80d0b7f41
Kristina <kristinaverma01@gmail.com>
2022-07-16 17:26:46 +1000

    Tilemap + Main Menu Functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     //public void Options(bool ToOptions)
